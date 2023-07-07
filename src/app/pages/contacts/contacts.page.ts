96c659cfca3d808758b91e0681ddf3f7b95c4e34
Luferat <contato@luferat.net>
2023-07-07 16:36:40 -0300

    fghjk,

--- src/app/pages/contacts/contacts.page.ts
+++ src/app/pages/contacts/contacts.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-contacts',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsPage implements OnInit {
 
+  env = environment;
+ 
   constructor() { }
 
   ngOnInit() { }
